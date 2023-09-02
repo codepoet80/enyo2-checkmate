6d9cef44f4a3509ed79b8e8cd4ad8db6454ffe81
codepoet80 <jon.wise@jonandnic.com>
2023-09-02 11:10:12 -0400

    bump version

--- .vscode/run.sh
+++ .vscode/run.sh
@@ -2,6 +2,6 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 echo copying latest common components
 #cp $DIR/../../webos-common/Enyo/Updater-Helper.js $DIR/../enyo-app/helpers/Updater.js
 $DIR/../build.sh webos
-palm-install $DIR/../bin/com.webosarchive.checkmatehd_1.1.0_all.ipk
+palm-install $DIR/../bin/com.webosarchive.checkmatehd_1.1.1_all.ipk
 palm-launch com.webosarchive.checkmatehd
 palm-log -f com.webosarchive.checkmatehd
